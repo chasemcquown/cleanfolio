65b2ec42048c9ac21838d03ae2aee59d08b162a5
chasem <cpmcquown@gmail.com>
2024-01-19 16:27:15 -0800

    Adds

--- script.js
+++ script.js
@@ -1,50 +1,40 @@
 // DESKTOP NAV LOGIC
 function isActive () {
-    if(window.location.href === 'file:///C:/Users/kmcqu/Desktop/cleanfolio/index.html' || 'https://chasemcquown.github.io/cleanfolio/index.html') {
+    if(window.location.href === 'file:///C:/Users/kmcqu/Desktop/cleanfolio/index.html') {
         document.querySelector(".nav-items a:nth-child(1)").style.borderBottom = '2px solid var(--Blue, #1032E9)'
         document.querySelector(".nav-items a:nth-child(2)").style.color = '#9A9A9F'
         document.querySelector(".nav-items a:nth-child(3)").style.color = '#9A9A9F'
         document.querySelector(".nav-items a:nth-child(4)").style.color = '#9A9A9F'
-        document.querySelector(".nav-items a:nth-child(5)").style.color = '#9A9A9F'
         //hover about
         document.querySelector(".nav-items a:nth-child(2)").addEventListener('mouseover', () => document.querySelector(".nav-items a:nth-child(2)").style.color = '#02030E')
         document.querySelector(".nav-items a:nth-child(2)").addEventListener('mouseover', () => document.querySelector(".nav-items a:nth-child(2)").style.transition = '0.3s')
         document.querySelector(".nav-items a:nth-child(2)").addEventListener('mouseleave', () => document.querySelector(".nav-items a:nth-child(2)").style.color = '#9A9A9F')
-        //hover work
+        //hover contact
         document.querySelector(".nav-items a:nth-child(3)").addEventListener('mouseover', () => document.querySelector(".nav-items a:nth-child(3)").style.color = '#02030E')
         document.querySelector(".nav-items a:nth-child(3)").addEventListener('mouseover', () => document.querySelector(".nav-items a:nth-child(3)").style.transition = '0.3s')
         document.querySelector(".nav-items a:nth-child(3)").addEventListener('mouseleave', () => document.querySelector(".nav-items a:nth-child(3)").style.color = '#9A9A9F')
-        //hover contact
+        //hover resume
         document.querySelector(".nav-items a:nth-child(4)").addEventListener('mouseover', () => document.querySelector(".nav-items a:nth-child(4)").style.color = '#02030E')
         document.querySelector(".nav-items a:nth-child(4)").addEventListener('mouseover', () => document.querySelector(".nav-items a:nth-child(4)").style.transition = '0.3s')
         document.querySelector(".nav-items a:nth-child(4)").addEventListener('mouseleave', () => document.querySelector(".nav-items a:nth-child(4)").style.color = '#9A9A9F')
-        //hover resume
-        document.querySelector(".nav-items a:nth-child(5)").addEventListener('mouseover', () => document.querySelector(".nav-items a:nth-child(5)").style.color = '#02030E')
-        document.querySelector(".nav-items a:nth-child(5)").addEventListener('mouseover', () => document.querySelector(".nav-items a:nth-child(5)").style.transition = '0.3s')
-        document.querySelector(".nav-items a:nth-child(5)").addEventListener('mouseleave', () => document.querySelector(".nav-items a:nth-child(5)").style.color = '#9A9A9F')
 
-    } if(window.location.href === 'file:///C:/Users/kmcqu/Desktop/cleanfolio/pages/about.html' || 'https://chasemcquown.github.io/cleanfolio/pages/about.html') {
+    } if(window.location.href === 'file:///C:/Users/kmcqu/Desktop/cleanfolio/pages/about.html') {
         document.querySelector(".nav-items a:nth-child(1)").style.color = '#9A9A9F'
         document.querySelector(".nav-items a:nth-child(2)").style.borderBottom = '2px solid var(--Blue, #1032E9)'
         document.querySelector(".nav-items a:nth-child(3)").style.color = '#9A9A9F'
         document.querySelector(".nav-items a:nth-child(4)").style.color = '#9A9A9F'
-        document.querySelector(".nav-items a:nth-child(5)").style.color = '#9A9A9F'
         //hover home
         document.querySelector(".nav-items a:nth-child(1)").addEventListener('mouseover', () => document.querySelector(".nav-items a:nth-child(1)").style.color = '#02030E')
         document.querySelector(".nav-items a:nth-child(1)").addEventListener('mouseover', () => document.querySelector(".nav-items a:nth-child(1)").style.transition = '0.3s')
         document.querySelector(".nav-items a:nth-child(1)").addEventListener('mouseleave', () => document.querySelector(".nav-items a:nth-child(1)").style.color = '#9A9A9F')
-        //hover work
+        //hover contact
         document.querySelector(".nav-items a:nth-child(3)").addEventListener('mouseover', () => document.querySelector(".nav-items a:nth-child(3)").style.color = '#02030E')
         document.querySelector(".nav-items a:nth-child(3)").addEventListener('mouseover', () => document.querySelector(".nav-items a:nth-child(3)").style.transition = '0.3s')
         document.querySelector(".nav-items a:nth-child(3)").addEventListener('mouseleave', () => document.querySelector(".nav-items a:nth-child(3)").style.color = '#9A9A9F')
-        //hover contact
+        //hover resume
         document.querySelector(".nav-items a:nth-child(4)").addEventListener('mouseover', () => document.querySelector(".nav-items a:nth-child(4)").style.color = '#02030E')
         document.querySelector(".nav-items a:nth-child(4)").addEventListener('mouseover', () => document.querySelector(".nav-items a:nth-child(4)").style.transition = '0.3s')
         document.querySelector(".nav-items a:nth-child(4)").addEventListener('mouseleave', () => document.querySelector(".nav-items a:nth-child(4)").style.color = '#9A9A9F')
-        //hover resume
-        document.querySelector(".nav-items a:nth-child(5)").addEventListener('mouseover', () => document.querySelector(".nav-items a:nth-child(5)").style.color = '#02030E')
-        document.querySelector(".nav-items a:nth-child(5)").addEventListener('mouseover', () => document.querySelector(".nav-items a:nth-child(5)").style.transition = '0.3s')
-        document.querySelector(".nav-items a:nth-child(5)").addEventListener('mouseleave', () => document.querySelector(".nav-items a:nth-child(5)").style.color = '#9A9A9F')
     }
 }
 
